1fae371e96e6436c4d33152a49371b0b2b6f85cf
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-07-13 02:35:22 +0530

    Update page.tsx

--- src/app/createPool/page.tsx
+++ src/app/createPool/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import CreateFatePoolForm from "@/components/Forms/CreateFatePool";
-import FooterWrapper from "@/components/layout/FooterWrapper";
 import Navbar from "@/components/layout/Navbar";
 
 export default function CreateFatePoolPage() {
@@ -11,7 +10,6 @@ export default function CreateFatePoolPage() {
         <CreateFatePoolForm />
       </div>
       <div className="h-[30vh] dark:bg-black bg-white"></div>
-      <FooterWrapper />
     </>
   );
 }
